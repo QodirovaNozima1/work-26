f4abfa689954c0a0bfc21a8dc7b9e727fcc535d5
QodirovaNozima1 <nozimaqodirova07@gmail.com>
2024-07-04 16:52:54 +0500

    work

--- js/app.js
+++ js/app.js
@@ -1,22 +1,39 @@
-const title = document.querySelector(".title")
-
-function clock() {
-// var weekend =["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
-//     var month = ["January", "February", "March", "April", "May", "June",
-//     "July", "August", "September", "October", "November", "December" ];
-
-//     var now = new Date()
-//     document.getElementById(`date`).innerHTML = (weekend[now.getDay()]+ " " + now.getDate()+ ` `+ month[now.month()]+ ` ` + now.getFullYear())
 
 
-let date = new Date()
-let hour = date.getHours()
-let minute = date.getMinutes()
-let second = date.getSeconds()
-let result = [hour,minute,second].map(i => i.toString().padStart(2,"0")).join(":")
-title.textContent = result
-}
-clock()
-setInterval(()=>{
-    clock()
-},1000)
+
+const clock = document.querySelector(".clock")
+        const day = document.querySelector(".day")
+        const kun = document.querySelector(".date")
+        const millisecond = document.querySelector(".millisecond");
+
+            function soat(){
+                let date = new Date()
+                let hour = date.getHours().toString().padStart(2, "0")
+                let minute = date.getMinutes().toString().padStart(2, "0")
+                let second = date.getSeconds().toString().padStart(2, "0")
+                // let millisecond = date.getMilliseconds().toString().padStart(2, "0")
+                // let day = date.getDay()
+                clock.textContent = `${hour}:${minute}:${second}`;
+                // millisecond.textContent = `${millisecond}`
+            }
+            soat()
+            setInterval(()=>{
+                soat()
+            },100);
+        
+        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        const weeks = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        function vaqt(){
+            let date = new Date()
+            let day = date.getDate()
+            let month = date.getMonth()
+            let year = date.getFullYear()
+            let kun = date.getDate()
+
+            day.textContent = `${month[month]}`;
+            kun.textContent = `${month} ${kun}, ${year}`;
+        }
+        vaqt()
+        setInterval(()=>{
+            vaqt()
+        },100);
